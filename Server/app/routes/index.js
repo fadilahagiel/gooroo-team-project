64366f568b7c55280ee505107e2ede44820a01c1
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-11-03 21:22:30 +0700

    student detail

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -4,6 +4,7 @@ const TransactionRouter = require("./transactionRouter");
 const TeacherRouter = require("./teacherRouter");
 const userRouter = require("./userRouter");
 const classRouter = require("./classRouter");
+const StudentRouter = require("./studentRouter");
 const authentication = require("../middlewares/authentication");
 
 router.use("/users", userRouter);
@@ -14,6 +15,7 @@ router.use(authentication);
 router.use("/wishlist", wishlistRouter);
 router.use("/transactions", TransactionRouter);
 router.use("/teachers", TeacherRouter);
+router.use("/students", StudentRouter);
 router.use("/class", classRouter);
 
 module.exports = router;
