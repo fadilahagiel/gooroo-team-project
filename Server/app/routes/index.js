87f332296133e6bf4046f66032123cf10510449b
fadilahagiel <fadilahagiel@gmail.com>
2022-11-03 17:37:27 +0700

    create class + schedule

--- Server/app/routes/index.js
+++ Server/app/routes/index.js
@@ -3,11 +3,13 @@ const wishlistRouter = require("./wishlistRouter");
 const TransactionRouter  = require('./transactionRouter')
 const TeacherRouter = require('./teacherRouter')
 const userRouter = require("./userRouter");
+const classRouter = require("./classRouter");
 
 router.use("/users", userRouter);
 router.use("/wishlist", wishlistRouter)
 router.use('/transactions', TransactionRouter)
 router.use('/teachers', TeacherRouter)
+router.use('/class', classRouter)
 
 module.exports = router
 
