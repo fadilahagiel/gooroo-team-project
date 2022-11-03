498242aba3ef2e1021b4ee3cc6ae7a8364c738c5
fadilahagiel <fadilahagiel@gmail.com>
2022-11-03 22:06:23 +0700

    classSchema, transactionSchema

--- Server/app/middlewares/errorHandlers.js
+++ Server/app/middlewares/errorHandlers.js
@@ -24,13 +24,13 @@ const errorHandlers = async (err, req, res, next) => {
   } else if (err.name == "forbidden") {
     code = 403;
     message = "forbidden";
-  } else if (error.name == "class not found") {
+  } else if (err.name == "class not found") {
     code = 404;
     message = `Class not found`;
-  } else if (error.name == "already") {
+  } else if (err.name == "already") {
     code = 400;
     message = "You already add this class to your wishlist";
-  } else if (error.name == "already collected") {
+  } else if (err.name == "already collected") {
     code = 400;
     message = "You already collected this class's profit";
   }
--- Server/app/routes/transactionRouter.js
+++ Server/app/routes/transactionRouter.js
@@ -2,7 +2,8 @@ const TransactionController = require('../controllers/TransactionController')
 const router = require('express').Router()
 
 router.post('/:classId', TransactionController.enterClass)
-router.patch('/:id', TransactionController.studentResponse)
-router.patch('/saldo/:classId', TransactionController.collectingTransaction)
+router.get('/:id', TransactionController.findOneTransaction)
+router.put('/:id', TransactionController.studentResponse)
+router.patch('/:classId', TransactionController.collectTransaction)
 
 module.exports = router
