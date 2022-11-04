f7addf66a51418db60f27baa017ed7ec1cddeadb
Giovanni Gabriel Septriadi <giovannigabriel619@gmail.com>
2022-11-04 21:24:11 +0700

    add wishlist schema,refactoring classcontroller and wishlistcontroller,assosiate class and subject

--- Server/services/app/models/subject.js
+++ Server/services/app/models/subject.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Subject extends Model {
     /**
@@ -11,14 +9,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Subject.hasMany(models.Class, { foreignKey: "SubjectId" });
     }
   }
-  Subject.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Subject',
-  });
+  Subject.init(
+    {
+      name: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Subject",
+    }
+  );
   return Subject;
 };
