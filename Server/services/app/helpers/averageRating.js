7b043040e5c6f75accd24b7a87945c7388fedf8e
fadilahagiel <fadilahagiel@gmail.com>
2022-11-06 00:07:49 +0700

    average rating

--- Server/services/app/helpers/averageRating.js
+++ Server/services/app/helpers/averageRating.js
@@ -5,7 +5,7 @@ async function classAverageRating (ClassId) {
     let transactions = await Transaction.findAll({ where: { ClassId: classFound.id } })
     transactions = transactions.filter((el) => el.rating != 0)
     let ratings = transactions.map(el => Number(el.rating))
-    const updatedRating = ratings.reduce((a, b) => a + b, 0) / ratings.length
+    const updatedRating = (ratings.reduce((a, b) => a + b, 0) / ratings.length).toFixed(2)
     return updatedRating
 }
 
@@ -15,7 +15,7 @@ async function teacherAverageRating(ClassId) {
     let classes = await Class.findAll({ where: { TeacherId: teacher.id } })
     classes = classes.filter((el) => el.averageRating != '0')
     let ratings = classes.map(el => Number(el.averageRating))
-    const updatedRating = ratings.reduce((a, b) => a + b, 0) / ratings.length
+    const updatedRating = (ratings.reduce((a, b) => a + b, 0) / ratings.length).toFixed(2)
     return {updatedRatingTeacher: updatedRating, TeacherId: teacher.id}
 }
 
