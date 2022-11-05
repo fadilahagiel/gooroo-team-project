16ff6afcc7278fc3053bf2852607529f1ec7a64c
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-11-06 00:12:55 +0700

    client-setup-login-register

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./src/screens/Home";
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+
+const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <stack.Screen name="Welcome" component={WelcomeScreen} />
+        <stack.Screen name="Login" component={Login} />
+        <stack.Screen name="Register" component={Register} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
