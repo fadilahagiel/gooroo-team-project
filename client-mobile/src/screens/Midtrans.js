2c134821cf72dc3fe0e2bea0933d6f4e3af6f7f1
Giovanni Gabriel Septriadi <giovannigabriel619@gmail.com>
2022-11-07 15:18:06 +0700

    installasi axios for mobile-client, wiring topup midtrans from client to server, change method topup from get to post

--- client-mobile/src/screens/Midtrans.js
+++ client-mobile/src/screens/Midtrans.js
@@ -1,15 +1,32 @@
 import { WebView } from "react-native-webview";
+import axios from "axios";
 
 export default function Midtrans({ navigation, route }) {
   const { url, price } = route.params;
-
+  const updateSaldo = async (prc) => {
+    try {
+      await axios({
+        method: "patch",
+        url: `https://335d-139-228-102-240.ap.ngrok.io/users/saldo`,
+        data: {
+          saldo: prc,
+        },
+        headers: {
+          access_token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InN0dWRlbnQiLCJpYXQiOjE2Njc4MDU4NjF9.PAOA-qpaxOvDcQl8w6lELJOkG-gI7e7Z3CoYuHWFjlo",
+        },
+      });
+      navigation.navigate("Profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <WebView
       source={{ uri: url }}
       onNavigationStateChange={(navState) => {
         if (navState.url.includes("success")) {
-          console.log(price, "tes sukses");
-          navigation.navigate("Profile");
+          updateSaldo(price);
         }
       }}
     />
