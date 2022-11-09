1ac3e419f15085d65dceaf5b2864d1a8ae06fb98
fadilahagiel <fadilahagiel@gmail.com>
2022-11-09 23:04:33 +0700

    testing

--- Server/services/app/routes/scheduleRouter.js
+++ Server/services/app/routes/scheduleRouter.js
@@ -3,6 +3,7 @@ const { AuthTeacher } = require("../middlewares/authorization");
 const router = require("express").Router();
 
 router.post("/:ClassId", AuthTeacher, ScheduleController.addSchedule);
+router.get("/:ClassId", ScheduleController.schedulesClass);
 router.delete("/:ScheduleId", ScheduleController.deleteSchedule);
 router.put("/:ScheduleId", ScheduleController.updateSchedule);
 
