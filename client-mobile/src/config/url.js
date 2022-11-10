4435dcfd6b9f18c4a0c8184b217b59c87aeb1e08
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-11-10 14:28:26 +0700

    done

--- client-mobile/src/config/url.js
+++ client-mobile/src/config/url.js
@@ -1,4 +1,3 @@
-export const serverUrl =
-  "https://4ff2-202-61-98-116.ap.ngrok.io";
+export const serverUrl = "https://4ff2-202-61-98-116.ap.ngrok.io";
 export const serverChat =
   "https://c70f-2001-448a-2042-93b9-550a-2fa5-c341-8c0f.ap.ngrok.io";
